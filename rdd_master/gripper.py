c38201bdcfc147df49c840879b6657c049a89804
Stephenyu113 <stephenyu113@outlook.com>
2019-06-06 20:13:35 -0400

    updated gripper_test

--- rdd_master/gripper.py
+++ rdd_master/gripper.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import rospy
-from rdda.msg import JointCommands
-from rdda.msg import JointStates
+import numpy as np
+from rdda_interface.msg import JointCommands
+from rdda_interface.msg import JointStates
+
 
 class Gripper:
 
@@ -17,27 +19,4 @@ class Gripper:
         """Obtain the status of the gripper."""
         self.status = states
         rospy.loginfo("Actual position[0]: %lf", self.status.act_pos[0])
-
-    def setGripperVel(self, ang_vel):
-        """Set angular velocity """
-        self.gripperPub.tg_pos = ang_vel
-
-"""
-    def closeGripper(self):
-        print 'Closing gripper ...'
-        cmd = JointCommands()
-        cmd.tg_pos[0] = 0
-        cmd.tg_pos[1] = 0
-        self.gripperPub.publish(cmd)
-        rospy.sleep(0.5)
-
-    def openGripper(self):
-        print 'Open gripper ...'
-        cmd = JointCommands()
-        cmd.tg_pos[0] = -0.5
-        cmd.tg_pos[1] = -0.5
-        rospy.sleep(0.5)
-"""
-
-
 
--- rdd_master/test_gripper.py
+++ rdd_master/test_gripper.py
@@ -2,27 +2,28 @@
 import rospy
 import numpy as np
 from gripper import Gripper
+from rdda_interface.msg import JointCommands
 
 
 def main():
-
-    try:
-        rospy.init_node('gripper_node', anonymous=True)
-        rospy.Rate(500)
-        run()
-    except rospy.ROSInterruptException:
-        pass
-
-
-def run():
     gripper = Gripper()
+    rospy.init_node('gripper_node', anonymous=True)
+    rate = rospy.Rate(500)
+    cmds = JointCommands()
     time_interval = 0.0
+    vel_ref = np.array([0.0, 0.0])
 
     while not rospy.is_shutdown():
-        vel_ref = -4.0 * np.sin(time_interval)
+        vel_ref[0] = -4.0 * np.sin(time_interval)
+        vel_ref[1] = 0.0
         time_interval += 0.5e-3
-        gripper.setGripperVel(vel_ref)
+        cmds.vel_sat = vel_ref
+        gripper.gripperPub.publish(cmds)
+        rate.sleep()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
